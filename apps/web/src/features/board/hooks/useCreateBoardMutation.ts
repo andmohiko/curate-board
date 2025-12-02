d15f9ef147d860136fa81d400093ba77a67d5c36
andmohiko <andmohiko@gmail.com>
2025-12-02 21:42:04 +0900

    feat: create board mutation

---
+++ apps/web/src/features/board/hooks/useCreateBoardMutation.ts
@@ -0,0 +1,61 @@
+import type { CreateBoardDto } from '@curate/common'
+import { useCallback, useState } from 'react'
+import type { EditBoardInputType } from '~/features/board/types'
+import { useToast } from '~/hooks/useToast'
+import { createBoardOperation } from '~/infrastructure/firestore/BoardOperations'
+import { serverTimestamp } from '~/lib/firebase'
+import { useFirebaseAuthContext } from '~/providers/FirebaseAuthProvider'
+import { errorMessage } from '~/utils/errorMessage'
+
+/**
+ * ボードを作成するカスタムフック
+ *
+ * @returns [作成関数, エラーメッセージ, ローディング状態]
+ */
+export const useCreateBoardMutation = (): [
+  (data: EditBoardInputType) => Promise<void>,
+  string | null,
+  boolean,
+] => {
+  const { uid } = useFirebaseAuthContext()
+  const { showErrorToast, showSuccessToast } = useToast()
+  const [error, setError] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const createBoard = useCallback(
+    async (data: EditBoardInputType) => {
+      if (!uid) {
+        showErrorToast('再度ログインしてください')
+        return
+      }
+
+      setIsLoading(true)
+      setError(null)
+
+      try {
+        const createBoardDto: CreateBoardDto = {
+          backgroundImageUrl: data.backgroundImageUrl ?? '',
+          createdAt: serverTimestamp,
+          items: data.items,
+          styleBackgroundColor: data.styleBackgroundColor ?? '#ffffff',
+          styleTextColor: data.styleTextColor ?? '#323232',
+          title: data.title,
+          updatedAt: serverTimestamp,
+          userId: uid,
+        }
+        await createBoardOperation(createBoardDto)
+        showSuccessToast('ボードを作成しました')
+      } catch (e) {
+        const errorMsg = errorMessage(e)
+        setError(errorMsg)
+        showErrorToast('ボードの作成に失敗しました', '再度お試しください')
+        throw e
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [uid, showErrorToast, showSuccessToast],
+  )
+
+  return [createBoard, error, isLoading]
+}
--- apps/web/src/features/board/types.ts
+++ apps/web/src/features/board/types.ts
@@ -1,5 +1,23 @@
+import { z } from 'zod'
+
 export type BoardItem = {
   label: string
   value: string
   imageUrl?: string
 }
+
+export const editBoardSchema = z.object({
+  backgroundImageUrl: z.string().optional(),
+  items: z.array(
+    z.object({
+      label: z.string(),
+      value: z.string(),
+      imageUrl: z.string().optional(),
+    }),
+  ),
+  styleBackgroundColor: z.string().optional(),
+  styleTextColor: z.string().optional(),
+  title: z.string(),
+})
+
+export type EditBoardInputType = z.infer<typeof editBoardSchema>
