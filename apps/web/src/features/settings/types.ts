22adf3d3b591594599252c7403352f6fef68abd8
andmohiko <andmohiko@gmail.com>
2025-12-05 00:44:40 +0900

    fix: username validation

--- apps/web/src/features/settings/types.ts
+++ apps/web/src/features/settings/types.ts
@@ -8,11 +8,11 @@ export const editProfileSchema = z.object({
   profileImageUrl: z.string().optional(),
   username: z
     .string()
-    .min(1, { message: '必須項目です' })
-    .max(20, { message: '20文字以内で入力してください' })
     .regex(/^[a-zA-Z0-9_]+$/, {
-      message: '半角英数字で入力してください',
-    }),
+      message: 'ユーザー名は英数字とアンダースコア( _ )のみで入力してください',
+    })
+    .min(5, { message: 'ユーザー名は5文字以上で入力してください' })
+    .max(15, { message: 'ユーザー名は15文字以内で入力してください' }),
 })
 
 export type EditProfileInputType = z.infer<typeof editProfileSchema>
